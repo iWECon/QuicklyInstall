9726f6237754311bb10c089fecec7e75d2bfb330
iWe <16604027+iWECon@users.noreply.github.com>
2019-11-27 17:10:22 +0800

    Update private-interface-pull.sh

--- private-interface-pull.sh
+++ private-interface-pull.sh
@@ -1,10 +1,10 @@
 path=/www/docker/interface
 
 if [ -f "/www/docker/interface" ];then
+    git clone https://github.com/iWECon/flask.git $path
+    cd $path
+else
     mkdir $path
     cd $path
     git pull
-else
-    git clone https://github.com/iWECon/flask.git $path
-    cd $path
 fi
